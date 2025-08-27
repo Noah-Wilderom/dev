6e8d06700f628c259a4fdad5a40897b7ad86d41a
Noah Wilderom <wilderomnoah@gmail.com>
2025-08-27 08:55:27 +0200

    wip

---
+++ scripts/kitty.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+
+ln -s ~/.config/kitty/kitty-themes/themes/Monokai_Soda.conf ~/.config/kitty/theme.conf
