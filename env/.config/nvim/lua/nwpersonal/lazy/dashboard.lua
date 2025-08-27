3147e10516295fa6c8417cb6be87cc20c3e65694
Noah Wilderom <wilderomnoah@gmail.com>
2025-08-27 10:47:20 +0200

    wip

--- env/.config/nvim/lua/nwpersonal/lazy/dashboard.lua
+++ env/.config/nvim/lua/nwpersonal/lazy/dashboard.lua
@@ -1,10 +1,32 @@
 return {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-        require('dashboard').setup {
-            -- config
-        }
-    end,
-    dependencies = {{'nvim-tree/nvim-web-devicons'}}
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "Files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f"
+					},
+					{
+						desc = " Keymaps",
+						group = "Keymaps",
+						action = "Telescope keymaps",
+						key = "k",
+					},
+				},
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
